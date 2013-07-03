c8f6872b7d1af4309bd4975b972590e8ceb037e1
Jason Frank <jfrank@guptamedia.com>
2013-07-03 15:42:12 -0400

    Initial import

---
+++ render/metadata.rb
@@ -0,0 +1,14 @@
+maintainer       "Gupta Media, LLC"
+maintainer_email "tech@guptamedia..com"
+license          "Copyright Gupta Media LLC. All rights reserved."
+description      "smartURL Render"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.0.0"
+
+#supports "ubuntu", "~> 10.04.0"
+
+#depends "app"
+#depends "web_apache"
+
+recipe "render::default", "Sets up the Render machine"
+
---
+++ render/recipes/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: render
+# Recipe:: default
+# #
+# # Copyright 2013, YOUR_COMPANY_NAME
+# #
+# # All rights reserved - Do Not Redistribute
+# #
+%w{openjdk-6-jre-headless ntp unzip}.each do |pkg|
+       	package pkg do
+	          action :install
+    end
+end
+
